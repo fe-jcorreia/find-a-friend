6addb3f618577f4a0a56fdf88819fb18af8383a4
fe-jcorreia <fe.jcorreia@gmail.com>
2024-03-27 09:46:10 -0300

    chore: add eslint + dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
+import { env } from "./env";
 
 const start = async () => {
   try {
-    await app.listen({ host: "0.0.0.0", port: 3000 });
-    console.log("HTTP Server running!")
+    await app.listen({ host: env.HOST, port: env.PORT });
+    console.log("HTTP Server running!");
   } catch (err) {
     app.log.error(err);
   }
